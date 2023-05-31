657df3011a0e488910484f1c442db17b5b985f18
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2023-05-31 13:42:25 +0200

    More stuff

--- pstb/bn_monolayer.py
+++ pstb/bn_monolayer.py
@@ -1,14 +1,22 @@
 import hamiltonian
+import numpy as np
+from scipy.constants import physical_constants as pyc
 
-    class bn_monolayer(hamiltonian):
+class bn_monolayer(hamiltonian):
 
-        def setup(self, parms = None):
-            self.s_dim=2
-            self.h_dim=2
-            if parms != None:
-                self.t  = parms[0]  # Hopping term
-                self.de = parms[1]  # One-site energy difference
-            else:
-                # default values taken from Phys. Rev. B 100, 195201 (2019)
-                self.t  = 2.92 *
-                self.de = 2.81 * 
+    def setup(self, parms = None):
+        self.s_dim=2
+        self.h_dim=2
+        if parms != None:
+            self.t  = parms[0]  # Hopping term in atomic units
+            self.de = parms[1]  # One-site energy difference in atomic units
+        else:
+            # default values taken from Phys. Rev. B 100, 195201 (2019)
+            self.t  = 2.92 * pyc["electron volt-hartree relationship"][0]
+            self.de = 2.81 * pyc["electron volt-hartree relationship"][0]
+
+        self.Hk=np.zeros((self.h_dim, self.h_dim),complex)
+
+    def eval_Hk(self, kpoint):
+        # kpoint is in crystal (reduced) units
+        Hk=
